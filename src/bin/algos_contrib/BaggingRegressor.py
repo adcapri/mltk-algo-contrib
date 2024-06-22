82b92135f781ac00c389e7bb6abc502966ba0353
adcapri <anndreasmall@gmail.com>
2024-06-22 18:30:49 -0400

    Update BaggingRegressor.py

--- src/bin/algos_contrib/BaggingRegressor.py
+++ src/bin/algos_contrib/BaggingRegressor.py
@@ -22,14 +22,27 @@ class BaggingRegressor(RegressorMixin, BaseAlgo):
 
         self.estimator = _BaggingRegressor(**out_params)
         
-
+    def summary(self, options):
+        if len(options) != 2:  # only model name and mlspl_limits
+            raise RuntimeError(
+                '"%s" models do not take options for summarization' % self.__class__.__name__
+            )
+        df = DataFrame(
+            {'feature': self.columns, 'importance': self.estimator.feature_importances_.ravel()}
+        )
+        return df
+        
     @staticmethod
     def register_codecs():
         from codec.codecs import SimpleObjectCodec, TreeCodec
 
-        codecs_manager.add_codec('algos.BaggingRegressor', 'BaggingRegressor', SimpleObjectCodec)
-        codecs_manager.add_codec('sklearn.ensemble.classes', 'BaggingRegressor', SimpleObjectCodec)
-        codecs_manager.add_codec('sklearn.tree.tree', 'DecisionTreeRegressor', SimpleObjectCodec)
-        codecs_manager.add_codec('sklearn.ensemble.weight_boosting', 'BaggingRegressor', SimpleObjectCodec)
-        codecs_manager.add_codec('sklearn.ensemble.bagging', 'BaggingRegressor', SimpleObjectCodec)
+        codecs_manager.add_codec(
+            'algos_contrib.BaggingRegressor', 'BaggingRegressor', SimpleObjectCodec
+        )
+        codecs_manager.add_codec(
+            'sklearn.ensemble._bagging', 'BaggingRegressor', SimpleObjectCodec
+        )
+        codecs_manager.add_codec(
+            'sklearn.tree._classes', 'DecisionTreeRegressor', SimpleObjectCodec
+        )
         codecs_manager.add_codec('sklearn.tree._tree', 'Tree', TreeCodec)
